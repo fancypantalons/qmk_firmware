86972b0fa4656e6c28a7b1588396ac9afb426453
Brett Kosinski <fancypantalons@gmail.com>
2019-01-26 16:31:31 -0700

    Set some nicer metadata.
    Proper values for manufacturer, product, and description so my dmesg
    looks cooler!

--- keyboards/handwired/handwirealpha/config.h
+++ keyboards/handwired/handwirealpha/config.h
@@ -7,9 +7,9 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    thebark
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define MANUFACTURER    The B Ark
+#define PRODUCT         Hand-wired Alpha
+#define DESCRIPTION     Hand-wired mechanical keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
