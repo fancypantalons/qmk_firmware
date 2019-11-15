ca64b2fec3e2c2dbe9cd564c4a3a42b8ab52b341
Brett Kosinski <fancypantalons@gmail.com>
2019-11-15 12:06:20 -0700

    Reset app key.

--- keyboards/wasdat/keymaps/tkl/keymap.c
+++ keyboards/wasdat/keymaps/tkl/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
             KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
 
-            KC_LCTL, KC_LGUI, KC_LALT, LT(1, KC_SPC), KC_RALT, KC_RGUI, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+            KC_LCTL, KC_LGUI, KC_LALT, LT(1, KC_SPC), KC_RALT, KC_RGUI, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
         ),
 	[1] = LAYOUT_tkl_ansi(
             RESET,   KC_NO, KC_NO, KC_NO, KC_NO, KC_BRID, KC_BRIU, KC_NO, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MUTE, 
